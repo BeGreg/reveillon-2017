608c0e2720811dc0fa049f2f42bf34644473c57b
gregory blain <gregory.blain@gmail.com>
2017-11-26 11:49:21 +0100

    controllers and views added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "creating participants"
+
+# Participant.create(name: "Blallain", number: "4", participate: true, sleepover: true )
+# Participant.create(name: "Thorlin", number: "4", participate: nil, sleepover: false )
+# Participant.create(name: "Valrand", number: "3", participate: nil, sleepover: false )
+# Participant.create(name: "Marchet", number: "4", participate: nil, sleepover: false )
+# Participant.create(name: "Fichet", number: "4", participate: nil, sleepover: false )
+# Participant.create(name: "Picaton", number: "3", participate: nil, sleepover: false )
+# Participant.create(name: "Bigoupillon", number: "2", participate: nil, sleepover: false )
+
+puts "crating accomodations"
+
+# Accomodation.create(name: "Suite", nb_persons: "2", participant_id:1)
+# Accomodation.create(name: "Lit Elise", nb_persons: "1", participant_id:1)
+Accomodation.create(name: "Lit Camille", nb_persons: "1", participant_id: 1)
+# Accomodation.create(name: "Chambre ami", nb_persons: "2", participant_id: nil)
+# Accomodation.create(name: "Canapé", nb_persons: "2", participant_id: nil)
+# Accomodation.create(name: "Matelas", nb_persons: "2", participant_id: nil)
+# Accomodation.create(name: "Lit enfant", nb_persons: "1", participant_id: nil)
+# Accomodation.create(name: "Place sous-sol 1", nb_persons: "1", participant_id: nil, comment: "amener son matelas à gonfler")
+# Accomodation.create(name: "Place sous-sol 2", nb_persons: "1", participant_id: nil,  comment: "amener son matelas à gonfler")
